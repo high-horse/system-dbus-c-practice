98ea50af71e849e2d71af3fb08e16be6f603b2da
akhilesh <akhileshadhikaria1@gmail.com>
2026-05-11 10:49:29 +0545

    battery health

--- src/ch3.c
+++ src/ch3.c
@@ -8,16 +8,30 @@
 #define BUS_IFACE "org.freedesktop.UPower.Device"
 #define TIMEOUT 1000
 
+typedef enum RETURN_TYPE {
+    RETURN_UINT32,
+    RETURN_INT32,
+    RETURN_DOUBLE,
+} RETURN_TYPE;
+
 static void abort_on_error(DBusError *error) ;
 static DBusMessage *create_property_get_message(const char *bus_name, const char *path, const char *iface, const char *propname);
 static double extract_double_from_variant(DBusMessage *reply, DBusError *error);
 static double get_double_property(DBusConnection *connection, const char *bus_name, const char *path, const char *iface, const char *propname, DBusError *error);
+
+static int get_i32_property(DBusConnection *connection, const char *bus_name, const char *path, const char *iface, const char *propname, DBusError *error);
+static int extract_i32_from_variant(DBusMessage *reply, DBusError *error);
+
+static unsigned int get_ui32_property(DBusConnection *connection, const char *bus_name, const char *path, const char *iface, const char *propname, DBusError *error);
+static unsigned int extract_ui32_from_variant(DBusMessage *reply, DBusError *error);
+
 
 int main() {
     DBusConnection *connection = NULL;
     DBusError error;
     double energy = 0;
     double fullEnergy = 0;
+    int chargeCycle = 0;
 
     dbus_error_init(&error);
 
@@ -30,6 +44,14 @@ int main() {
     fullEnergy = get_double_property(connection, BUS_NAME, BUS_PATH, BUS_IFACE, "EnergyFull", &error);
     abort_on_error(&error);
 
+    chargeCycle = get_i32_property(connection, BUS_NAME, BUS_PATH, BUS_IFACE, "ChargeCycles", &error);
+    abort_on_error(&error);
+
+    double health = get_double_property(connection, BUS_NAME, BUS_PATH, BUS_IFACE, "Capacity", &error);
+    abort_on_error(&error); 
+
+
+    printf("charge cycle %d\tbattery health %.2f\n", chargeCycle, health);
     printf("energy  %lf \n", (energy * 100) / fullEnergy);
     return EXIT_SUCCESS;
 }
@@ -62,19 +84,24 @@ static double get_double_property(
 
     if(dbus_error_is_set(&myerror)) {
         dbus_move_error(&myerror, error);
-        return EXIT_FAILURE;
+        return -1;
     }
 
     result = extract_double_from_variant(replyMessage, &myerror);
     if(dbus_error_is_set(&myerror)){
         dbus_move_error(&myerror, error);
-        return EXIT_FAILURE;
+        return -1;
     }
 
     dbus_message_unref(replyMessage);
     return result;
 }
 
+/*
+    NOTE: the property are to be accessed with generic `Properties` interface like `UPower.Properties` get method
+    even though the property lies in other interface like `UPower.Device`
+    pass the property to args with append_args 
+*/
 
 static DBusMessage *create_property_get_message(
     const char *bus_name, 
@@ -100,12 +127,128 @@ static double extract_double_from_variant(DBusMessage *reply, DBusError *error){
 
     if(DBUS_TYPE_VARIANT != dbus_message_iter_get_arg_type(&iter)){
         dbus_set_error_const(error, "reply_should_be_variant", "this message hasn't a variant message type");
-        return 0;
+        return -1;
     }
 
     dbus_message_iter_recurse(&iter, &sub) ;
     if(DBUS_TYPE_DOUBLE != dbus_message_iter_get_arg_type(&sub)){
         dbus_set_error(error, "variant_should be double", "the reply message must be double content");
+        return -1;
+    }
+
+    dbus_message_iter_get_basic(&sub, &result);
+    return result;
+}
+
+
+static int get_i32_property(
+    DBusConnection *connection, 
+    const char *bus_name, 
+    const char *path, 
+    const char *iface, 
+    const char *propname, 
+    DBusError *error
+) {
+    DBusError myError;
+    int result = 0;
+    DBusMessage *queryMessage = NULL;
+    DBusMessage *replyMessage = NULL;
+
+    dbus_error_init(&myError);
+    queryMessage = create_property_get_message(bus_name, path, iface, propname);
+    replyMessage  = dbus_connection_send_with_reply_and_block(connection, queryMessage, TIMEOUT, &myError);
+    dbus_message_unref(queryMessage);
+
+    if(dbus_error_is_set(&myError)) {
+        dbus_move_error(&myError, error);
+        return -1;
+    }
+
+    result = extract_i32_from_variant(replyMessage, &myError);
+    if(dbus_error_is_set(&myError)) {
+        dbus_move_error(&myError, error);
+        return -1;
+    }
+
+    dbus_message_unref(replyMessage);
+    return result;
+}
+
+
+
+static int extract_i32_from_variant(DBusMessage *reply, DBusError *error) {
+    DBusMessageIter iter;
+    DBusMessageIter sub;
+    int result ;
+
+    dbus_message_iter_init(reply, &iter);
+
+    if(DBUS_TYPE_VARIANT != dbus_message_iter_get_arg_type(&iter)){
+        dbus_set_error_const(error, "reply_should_be_variant", "this message hasn't a variant type");
+        return -1;
+    }
+
+    dbus_message_iter_recurse(&iter, &sub);
+    if(DBUS_TYPE_INT32 != dbus_message_iter_get_arg_type(&sub)) {
+        dbus_set_error(error, "variant_shoule_be_int32", "the reply message must be int32 content");
+        return -1;
+    }
+
+    dbus_message_iter_get_basic(&sub, &result);
+    return result;
+}
+
+
+static unsigned int get_ui32_property(
+    DBusConnection *connection, 
+    const char *bus_name, 
+    const char *path, 
+    const char *iface, 
+    const char *propname, 
+    DBusError *error
+) {
+    DBusError myError;
+    int result = 0;
+    DBusMessage *queryMessage = NULL;
+    DBusMessage *replyMessage = NULL;
+
+    dbus_error_init(&myError);
+    queryMessage = create_property_get_message(bus_name, path, iface, propname);
+    replyMessage  = dbus_connection_send_with_reply_and_block(connection, queryMessage, TIMEOUT, &myError);
+    dbus_message_unref(queryMessage);
+
+    if(dbus_error_is_set(&myError)) {
+        dbus_move_error(&myError, error);
+        return 0;
+    }
+
+    result = extract_ui32_from_variant(replyMessage, &myError);
+    if(dbus_error_is_set(&myError)) {
+        dbus_move_error(&myError, error);
+        return 0;
+    }
+
+    dbus_message_unref(replyMessage);
+    return result;
+}
+
+
+
+static unsigned int extract_ui32_from_variant(DBusMessage *reply, DBusError *error) {
+    DBusMessageIter iter;
+    DBusMessageIter sub;
+    unsigned int result ;
+
+    dbus_message_iter_init(reply, &iter);
+
+    if(DBUS_TYPE_VARIANT != dbus_message_iter_get_arg_type(&iter)){
+        dbus_set_error_const(error, "reply_should_be_variant", "this message hasn't a variant type");
+        return 0;
+    }
+
+    dbus_message_iter_recurse(&iter, &sub);
+    if(DBUS_TYPE_UINT32 != dbus_message_iter_get_arg_type(&sub)) {
+        dbus_set_error(error, "variant_shoule_be_uint32", "the reply message must be uint32 content");
         return 0;
     }
 
